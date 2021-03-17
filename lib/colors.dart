0b955e104a58552e4a575863ec7db8b24788e73b
Denis Plotnikov <denis.plotnikov.1994@gmail.com>
2021-03-18 00:08:02 +0300

    buttons

---
+++ lib/colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ThemeColors {
+  
+  static Color babyBlue = const Color(0xffC3E0E5);
+  static Color darkBlue = const Color(0xff274472);
+  static Color blueGray = const Color(0xff5885AF);
+  static Color midnightBlue = const Color(0xff41729F);
+ 
+}
